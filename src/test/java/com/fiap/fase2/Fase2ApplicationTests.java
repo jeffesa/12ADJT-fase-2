8bfefc4141d29f0197065304e5fb7ff06ed1d769
Jefferson Santos <jeffesa@gmail.com>
2026-05-30 01:14:47 -0300

    feat: TASK-001 setup projeto Spring Boot 3.2.3 com Java 17

---
+++ src/test/java/com/fiap/fase2/Fase2ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fiap.fase2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class Fase2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
