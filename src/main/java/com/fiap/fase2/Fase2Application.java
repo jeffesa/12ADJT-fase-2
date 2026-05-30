705f12f390a448c80e00eeafee775f5d78f2fe73
Jefferson Santos <jeffesa@gmail.com>
2026-05-30 01:24:48 -0300

    feat: TASK-001 - Setup projeto Spring Boot 3.2.3 (#51)
    * feat: TASK-001 setup projeto Spring Boot 3.2.3 com Java 17
    
    * fix: desativa jacoco:check temporariamente (ativado na TASK-030)

---
+++ src/main/java/com/fiap/fase2/Fase2Application.java
@@ -0,0 +1,12 @@
+package com.fiap.fase2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fase2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fase2Application.class, args);
+    }
+}
